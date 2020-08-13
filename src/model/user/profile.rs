5d25b6deb3a10133ce1cccd911e643770fa5e89b
FreeFly0125 <simonbergman516@gmail.com>
2020-08-13 17:48:52 +0200

    Update rustdoc references to renamed struct
    `Profile` was called `User` in GDCF

--- src/model/user/profile.rs
+++ src/model/user/profile.rs
@@ -43,37 +43,37 @@ impl Display for Twitter<'_> {
 /// `15`, `27`, `32`, `33`, `34`, `35`, `36`, `37`, `38`, `39`, `40`, `41`, `42`, `47`
 #[derive(Debug, Eq, PartialEq, Clone, Serialize, Deserialize)]
 pub struct Profile<'a> {
-    /// The [`User`]'s name
+    /// The [`Profile`]'s name
     ///
     /// ## GD Internals:
     /// This value is provided at index `1`.
     pub name: Cow<'a, str>,
 
-    /// The [`User`]'s unique user ID
+    /// The [`Profile`]'s unique user ID
     ///
     /// ## GD Internals:
     /// This value is provided at index `2`
     pub user_id: u64,
 
-    /// The amount of stars this [`User`] has collected.
+    /// The amount of stars this [`Profile`] has collected.
     ///
     /// ## GD Internals:
     /// This value is provided at index `3`
     pub stars: u32,
 
-    /// The demons of stars this [`User`] has beaten.
+    /// The demons of stars this [`Profile`] has beaten.
     ///
     /// ## GD Internals:
     /// This value is provided at index `4`
     pub demons: u16,
 
-    /// The amount of creator points this [`User`] was awarded.
+    /// The amount of creator points this [`Profile`] was awarded.
     ///
     /// ## GD Internals:
     /// This value is provided at index `8`
     pub creator_points: u16,
 
-    /// This [`User`]'s primary color
+    /// This [`Profile`]'s primary color
     ///
     /// ## GD Internals:
     /// This value is provided at index `10`. The game internally assigned each color some really
@@ -82,25 +82,25 @@ pub struct Profile<'a> {
     /// representation.
     pub primary_color: Color,
 
-    /// This [`User`]'s secondary color
+    /// This [`Profile`]'s secondary color
     ///
     /// ## GD Internals:
     /// This value is provided at index `11`. Same things as above apply
     pub secondary_color: Color,
 
-    /// The amount of secret coins this [`User`] has collected.
+    /// The amount of secret coins this [`Profile`] has collected.
     ///
     /// ## GD Internals:
     /// This value is provided at index `13`
     pub secret_coins: u8,
 
-    /// The [`User`]'s unique account ID
+    /// The [`Profile`]'s unique account ID
     ///
     /// ## GD Internals:
     /// This value is provided at index `16`
     pub account_id: u64,
 
-    /// The amount of user coins this [`User`] has collected.
+    /// The amount of user coins this [`Profile`] has collected.
     ///
     /// ## GD Internals:
     /// This value is provided at index `17`
@@ -116,55 +116,55 @@ pub struct Profile<'a> {
     /// This value is provided at index `19`
     pub index_19: Cow<'a, str>,
 
-    /// The link to the [`User`]'s [YouTube](https://youtube.com) channel, if provided
+    /// The link to the [`Profile`]'s [YouTube](https://youtube.com) channel, if provided
     ///
     /// ## GD Internals
     /// This value is provided at index `20`. The value provided is only the `username` section of an `https://www.youtube.com/user/{username}` URL
     pub youtube_url: Option<Youtube<'a>>,
 
-    /// The 1-based index of the cube this [`User`] currently uses. Indexing of icons starts at the
+    /// The 1-based index of the cube this [`Profile`] currently uses. Indexing of icons starts at the
     /// top left corner and then goes left-to-right and top-to-bottom
     ///
     /// ## GD Internals:
     /// This value is provided at index `21`
     pub cube_index: u16,
 
-    /// The 1-based index of the ship this [`User`] currently uses. Indexing of icons starts at the
+    /// The 1-based index of the ship this [`Profile`] currently uses. Indexing of icons starts at the
     /// top left corner and then goes left-to-right and top-to-bottom
     ///
     /// ## GD Internals:
     /// This value is provided at index `22`
     pub ship_index: u8,
 
-    /// The 1-based index of the ball this [`User`] currently uses. Indexing of icons starts at the
+    /// The 1-based index of the ball this [`Profile`] currently uses. Indexing of icons starts at the
     /// top left corner and then goes left-to-right and top-to-bottom
     ///
     /// ## GD Internals:
     /// This value is provided at index `23`
     pub ball_index: u8,
 
-    /// The 1-based index of the UFO this [`User`] currently uses. Indexing of icons starts at the
+    /// The 1-based index of the UFO this [`Profile`] currently uses. Indexing of icons starts at the
     /// top left corner and then goes left-to-right and top-to-bottom
     ///
     /// ## GD Internals:
     /// This value is provided at index `24`
     pub ufo_index: u8,
 
-    /// The 1-based index of the wave this [`User`] currently uses. Indexing of icons starts at the
+    /// The 1-based index of the wave this [`Profile`] currently uses. Indexing of icons starts at the
     /// top left corner and then goes left-to-right and top-to-bottom
     ///
     /// ## GD Internals:
     /// This value is provided at index `25`
     pub wave_index: u8,
 
-    /// The 1-based index of the robot this [`User`] currently uses. Indexing of icons starts at the
+    /// The 1-based index of the robot this [`Profile`] currently uses. Indexing of icons starts at the
     /// top left corner and then goes left-to-right and top-to-bottom
     ///
     /// ## GD Internals:
     /// This value is provided at index `26`
     pub robot_index: u8,
 
-    /// Values indicating whether this [`User`] has glow activated or not.
+    /// Values indicating whether this [`Profile`] has glow activated or not.
     ///
     /// ## GD Internals:
     /// This value is provided at index `28`, as an integer
@@ -175,7 +175,7 @@ pub struct Profile<'a> {
     /// This value is provided at index `29`
     pub index_29: Cow<'a, str>,
 
-    /// This [`User`]'s global rank. [`None`] if he is banned or not ranked.
+    /// This [`Profile`]'s global rank. [`None`] if he is banned or not ranked.
     ///
     /// ## GD Internals:
     /// This value is provided at index `30`. For unranked/banned users it's `0`
@@ -186,39 +186,39 @@ pub struct Profile<'a> {
     /// This value is provided at index `31`
     pub index_31: Cow<'a, str>,
 
-    /// The 1-based index of the spider this [`User`] currently uses. Indexing of icons starts at
+    /// The 1-based index of the spider this [`Profile`] currently uses. Indexing of icons starts at
     /// the top left corner and then goes left-to-right and top-to-bottom
     ///
     /// ## GD Internals:
     /// This value is provided at index `43`
     pub spider_index: u8,
 
-    /// The link to the [`User`]'s [Twitter](https://twitter.com) account, if provided
+    /// The link to the [`Profile`]'s [Twitter](https://twitter.com) account, if provided
     ///
     /// ## GD Internals
     /// This value is provided at index `44`. The value provided is only the `username` section of an `https://www.twitter.com/{username}` URL
     pub twitter_url: Option<Twitter<'a>>,
 
-    /// The link to the [`User`]'s [Twitch](https://twitch.tv) channel, if provided
+    /// The link to the [`Profile`]'s [Twitch](https://twitch.tv) channel, if provided
     ///
     /// ## GD Internals
     /// This value is provided at index `45`. The value provided is only the `username` section of an `https://twitch.tv/{username}` URL
     pub twitch_url: Option<Twitch<'a>>,
 
-    /// The amount of diamonds this [`User`] has collected.
+    /// The amount of diamonds this [`Profile`] has collected.
     ///
     /// ## GD Internals:
     /// This value is provided at index `46`
     pub diamonds: u16,
 
-    /// The 1-based index of the death-effect this [`User`] currently uses. Indexing of icons
+    /// The 1-based index of the death-effect this [`Profile`] currently uses. Indexing of icons
     /// starts at the top left corner and then goes left-to-right and top-to-bottom
     ///
     /// ## GD Internals:
     /// This value is provided at index `48`
     pub death_effect_index: u8,
 
-    /// The level of moderator this [`User`] is
+    /// The level of moderator this [`Profile`] is
     ///
     /// ## GD Internals:
     /// This value is provided at index `49`
