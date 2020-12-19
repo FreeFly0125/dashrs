7315ef65ee9b220572d5766b23a0dab317d93a18
FreeFly0125 <simonbergman516@gmail.com>
2020-12-19 10:57:47 +0000

    CI: Automatic Rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -37,4 +37,4 @@ pub mod response;
 pub(crate) mod serde;
 pub mod util;
 
-pub use crate::serde::{Base64Decoded, DeError, HasRobtopFormat, PercentDecoded, SerError, Thunk, ThunkContent, ProcessError};
+pub use crate::serde::{Base64Decoded, DeError, HasRobtopFormat, PercentDecoded, ProcessError, SerError, Thunk, ThunkContent};
