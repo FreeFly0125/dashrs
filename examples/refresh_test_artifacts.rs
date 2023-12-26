4fded82b8ce39ee204b2f7e87c08bfc6a47935ca
FreeFly0125 <simonbergman516@gmail.com>
2023-12-26 01:30:20 +0000

    Have test artifacts be generated by an "example"
    Each artifact consists of two files, both generated at the same time: A
    raw response from the Geometry Dash servers, and a serde_json dump of
    the processed version of that response (as dash-rs would interpret it at
    the time of creating the artifacts). Each test should then dynamically
    disocver all artifacts of a given type, assert their consistency (e.g.
    processing the response still gives the same dash-rs structure), and can
    then do round trip testing using these artifacts.

--- examples/refresh_test_artifacts.rs
+++ examples/refresh_test_artifacts.rs
@@ -1,28 +1,72 @@
-use dash_rs::request::level::LevelRequest;
+use std::{fs::OpenOptions, path::Path};
+
+use dash_rs::{request::{level::LevelRequest, user::UserRequest}, response::{parse_download_gj_level_response, parse_get_gj_user_info_response}};
 use reqwest::{
     header::{HeaderMap, CONTENT_TYPE},
     Client, Response,
 };
+use serde::Serialize;
 
 #[tokio::main]
 async fn main() {
+    // We want debug logging so that it tells us about unmapped indices
+    env_logger::builder().filter_level(log::LevelFilter::Debug).init();
+
     let http_client = Client::new();
 
-    let download_time_pressure = LevelRequest::new(897837);
-    let response = make_request(&http_client, &download_time_pressure.to_url(), download_time_pressure.to_string()).await;
-    std::fs::write(
-        "tests/data/897837_time_pressure_gjdownload_response",
-        &response.text().await.unwrap().split('#').next().unwrap(),
-    )
-    .unwrap();
+    let artifacts_path = Path::new("tests").join("artifacts");
+    let artifacts_backup = Path::new("tests").join("artifacts_backup");
 
-    let download_dark_realm = LevelRequest::new(11774780);
-    let response = make_request(&http_client, &download_dark_realm.to_url(), download_dark_realm.to_string()).await;
-    std::fs::write(
-        "tests/data/11774780_dark_realm_gjdownload_response",
-        &response.text().await.unwrap().split('#').next().unwrap(),
-    )
-    .unwrap();
+    std::fs::rename(&artifacts_path, &artifacts_backup).unwrap();
+
+    println!("Downloading levels");
+
+    let levels_path = artifacts_path.join("level");
+    let levels_to_download = [897837 /* time pressure by aeonair */, 11774780 /* dark realm by stardust1971 */];
+
+    for level_id in levels_to_download {
+        let request = LevelRequest::new(level_id);
+        let response = make_request(&http_client, &request.to_url(), request.to_string()).await;
+        let response_text = response.text().await.unwrap();
+        let level = parse_download_gj_level_response(&response_text).unwrap();
+
+        let level_artifact_path = levels_path.join(level_id.to_string());
+        let _ = std::fs::create_dir_all(&level_artifact_path);
+
+        std::fs::write(level_artifact_path.join("raw"), response_text.split('#').next().unwrap()).unwrap();
+        dump_deserialized_artifact(level_artifact_path, &level);
+    }
+
+    println!("Downloading profiles");
+
+    let profiles_path = artifacts_path.join("profile");
+    let profiles_to_download = [8451 /* stardust1971 */];
+
+    for account_id in profiles_to_download {
+        let request = UserRequest::new(account_id);
+        let response = make_request(&http_client, &request.to_url(), request.to_string()).await;
+        let response_text = response.text().await.unwrap();
+
+        let profile = parse_get_gj_user_info_response(&response_text).unwrap();
+
+        let profile_artifact_path = profiles_path.join(account_id.to_string());
+        let _ = std::fs::create_dir_all(&profile_artifact_path);
+
+        std::fs::write(profile_artifact_path.join("raw"), &response_text).unwrap();
+        dump_deserialized_artifact(profile_artifact_path, &profile);
+    }
+
+    std::fs::remove_dir_all(&artifacts_backup).unwrap();
+}
+
+fn dump_deserialized_artifact<S: Serialize>(dest: impl AsRef<Path>, artifact: &S) {
+    let mut file = OpenOptions::new()
+        .create(true)
+        .truncate(true)
+        .write(true)
+        .open(dest.as_ref().join("processed"))
+        .unwrap();
+    serde_json::to_writer_pretty(&mut file, artifact).unwrap();
 }
 
 async fn make_request(client: &Client, endpoint: &str, data: String) -> Response {
