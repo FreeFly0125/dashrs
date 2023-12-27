9de3342427fb58b08dffcdd9da0c72fa34250e12
FreeFly0125 <simonbergman516@gmail.com>
2023-12-27 01:22:40 +0000

    Correctly type Profile::moons as u32

--- src/model/user/profile.rs
+++ src/model/user/profile.rs
@@ -215,7 +215,7 @@ pub struct Profile<'a> {
 
     /// The number of moons this [`Profile`] has collected
     #[dash(index = 52)]
-    pub moons: Cow<'a, str>,
+    pub moons: u32,
 
     /// The 1-based index of the swing this [`Profile`] currently uses. Indexing of icons starts at
     /// the top left corner and then goes left-to-right and top-to-bottom
